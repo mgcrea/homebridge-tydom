3a396c87b5e8c4cfb156aa6e7565e0ae59a1be9a
Olivier Louvignes <olivier@mgcrea.io>
2020-09-01 18:11:32 +0200

    feat(accessories): add support for garage_door category (related to #29)

--- src/utils/tydom.ts
+++ src/utils/tydom.ts
@@ -120,6 +120,7 @@ const LEGACY_SUPPORTED_CATEGORIES_MAP: Record<string, Categories> = {
   alarm: Categories.SECURITY_SYSTEM,
   belmDoor: Categories.DOOR,
   gate: Categories.GARAGE_DOOR_OPENER,
+  garage_door: Categories.GARAGE_DOOR_OPENER,
   hvac: Categories.THERMOSTAT,
   light: Categories.LIGHTBULB,
   shutter: Categories.WINDOW_COVERING,
@@ -132,6 +133,7 @@ const ENDPOINTS_SIGNATURES_CATEGORIES: Record<string, Categories> = {
   'belmDoor:fb935867933d89b3058f09384f76fd63f3defb18cfb3172f60fa9f4f237f748b': Categories.DOOR, // @mgcrea (MDO)
   'conso:16804a9994bce28275150db329a9c0b931ef7f20608c1a3d2ff248f58569f0d3': Categories.SENSOR, // @maaxleop (STE 2000)
   'gate:83b0912c6fe14622219522922ea0347dcbf86bf9cfd3346a2eca8eac70ca8260': Categories.GARAGE_DOOR_OPENER, // @mgcrea (TYXIA 4620)
+  'garage_door:83b0912c6fe14622219522922ea0347dcbf86bf9cfd3346a2eca8eac70ca8260': Categories.GARAGE_DOOR_OPENER, // @Benzoiiit (TYXIA 4620)
   'hvac:1bab47d1dd7e898b5dc2e9867b14dfb8bc9272c4cb0b5d1221da962d43a6ffb4': Categories.THERMOSTAT, // @mgcrea (RF4890)
   'light:449e2a60377094cde10224cee91d378fb0ae373ae6ceea0ac2cbc1ed011bffa7': Categories.LIGHTBULB, // @mgcrea (TYXIA 5610, TYXIA 6610)
   'light:fce45085835f4f2790ea3b17d208b5ace34935444d2535e75ba3f0a2ce86de5f': Categories.LIGHTBULB, // @mgcrea (TYXIA 5650)
