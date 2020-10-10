f77f2f37760449e2476ad12d80b9c2cae3bd33ce
Olivier Louvignes <olivier@mgcrea.io>
2020-10-10 20:27:59 +0200

    chore(release): cut the 0.19.5 release

--- src/utils/tydom.ts
+++ src/utils/tydom.ts
@@ -14,6 +14,7 @@ import assert from '../utils/assert';
 import TydomClient from 'tydom-client';
 import debug from './debug';
 import {sha256Sync} from './hash';
+import {URLSearchParams} from 'url';
 
 type DataOperation = {
   promise: Promise<unknown> | null;
