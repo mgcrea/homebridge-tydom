ae3a857854819dddba65c2aff62a7295a41c34b2
Olivier Louvignes <olivier@mgcrea.io>
2023-08-22 20:38:57 +0200

    chore(controller): add verbose error on update handling

---
+++ src/utils/error.ts
@@ -0,0 +1,12 @@
+const errorReplacer = (key: string, value: unknown) => {
+  if (value instanceof Error) {
+    return {
+      name: value.name,
+      message: value.message,
+      stack: value.stack
+    };
+  }
+  return `${value}`;
+};
+
+export const stringifyError = (err: Error | unknown) => JSON.stringify(err, errorReplacer);
