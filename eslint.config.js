35491521dabe6ea2dc894186122096922af4ecca
Olivier Louvignes <olivier@mgcrea.io>
2026-02-15 21:21:41 +0100

    style: apply formatting and linting configuration updates
    - Update .gitignore with dist/ directory
    - Apply prettier formatting to eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-import baseConfig from '@mgcrea/eslint-config-node';
+import baseConfig from "@mgcrea/eslint-config-node";
 
 /**
  * Eslint configuration
@@ -11,8 +11,30 @@ export default [
   {
     languageOptions: {
       parserOptions: {
-        project: ['./tsconfig.json']
-      }
-    }
-  }
+        project: ["./tsconfig.json"],
+      },
+    },
+    rules: {
+      // Homebridge uses callback-based async patterns that trigger this rule
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        {
+          checksVoidReturn: {
+            arguments: false,
+          },
+        },
+      ],
+      // Allow unused vars prefixed with _
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { varsIgnorePattern: "^_", argsIgnorePattern: "^_", ignoreRestSiblings: true },
+      ],
+      // Template expressions with unknown types are common in debug logging
+      "@typescript-eslint/restrict-template-expressions": "warn",
+      // Non-null assertions are used intentionally after Map.has() checks
+      "@typescript-eslint/no-non-null-assertion": "warn",
+      // Unnecessary conditions can occur due to runtime vs compile-time type differences
+      "@typescript-eslint/no-unnecessary-condition": "warn",
+    },
+  },
 ];
