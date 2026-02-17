5ea6078fa69eaeae9a754f24df6fb225076b1d36
botho <sbothorel@free.fr>
2026-02-17 12:53:19 +0100

    fix

--- src/utils/basic.ts
+++ src/utils/basic.ts
@@ -7,7 +7,7 @@ export const sameArrays = (source: unknown[], array: unknown[]): boolean =>
   source.length === array.length && xor(source, array).length === 0;
 
 export const asNumber = (maybeNumber: string | number | boolean | null | undefined): number =>
-    parseInt(`${maybeNumber}`, 10);
+  parseInt(`${maybeNumber}`, 10);
 
 const timeouts = new Map<string, { timeout: NodeJS.Timeout; reject: () => void }>();
 export const waitFor = async (key: string, ms: number): Promise<void> => {
